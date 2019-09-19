0fd05750fbfae7cbbab0de015636d2bb92310431
Mansoor <mansoor@mansoor-mbp.home>
2019-09-18 20:17:57 -0700

    remove bg from service update command

--- aws.sh
+++ aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # switch to v2
-aws ecs update-service --cluster test-fargate --service nsoor-bg --task-definition nsoor-task:5
+aws ecs update-service --cluster test-fargate --service nsoor --task-definition nsoor-task:5
 
 # switch to v1
-aws ecs update-service --cluster test-fargate --service nsoor-bg --task-definition nsoor-task:4
+aws ecs update-service --cluster test-fargate --service nsoor --task-definition nsoor-task:4
